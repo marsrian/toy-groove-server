8194a8b6e7e3b99e0a8c0724a1d2bcca53dbfba6
marsrian <marsrian40@gmail.com>
2023-05-17 22:30:03 +0600

    toy-marketplace-server-start

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Toy Marketplace is running!');
+})
+
+app.listen(port, () =>{
+    console.log(`Toy Marketplace is running on: ${port}`)
+})
