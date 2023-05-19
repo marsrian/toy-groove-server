0f2ab22c5be160278232f2183089b3816c215654
marsrian <marsrian40@gmail.com>
2023-05-19 12:39:11 +0600

    toy post added

--- index.js
+++ index.js
@@ -10,6 +10,59 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qabixji.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const toysCollection = client.db("allToys").collection("toy");
+
+    // ...................Toys.....................//
+    // Create Toys Post:
+    app.post("/toys", async (req, res) => {
+        const newToy = req.body;
+        const result = await toysCollection.insertOne(newToy);
+        res.send(result);
+      });
+
+    // Read All Toys:
+    app.get("/toys", async (req, res) => {
+        const result = await toysCollection.find().limit(20).toArray();
+        res.send(result);
+      });
+
+    // Read Single Toy:
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
     res.send('Toy Marketplace is running!');
 })
